e78331ced9535128e35523dc4a5c57fa86b68328
ViniciusLelis <vinivictor1996@gmail.com>
2021-10-11 11:29:32 -0300

    Layout for Login activity added. Small changes to the initial account activity layout.

---
+++ app/src/main/java/com/lelissoftware/takeme/account/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.lelissoftware.takeme.account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lelissoftware.takeme.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
